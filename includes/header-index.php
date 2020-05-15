949755a16b661aa32d663f0fdeb140fc287f7b4e
K1N0 <edisonupb2013@gmail.com>
2020-05-15 17:14:14 -0500

    ajustestres

--- includes/header-index.php
+++ includes/header-index.php
@@ -4,7 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="Hoja de Vida - John Edison Múnera Bedoya">
+    <meta property="og:type" content="Web">
+    <meta property="og:url" content="https://edison-munera.herokuapp.com/">
+    <meta property="og:image" content="https://edison-munera.herokuapp.com/assets/img/img-into.jpg">
+    <meta property="og:local" content="es_LA">
     <title>Hoja de Vida - John Edison Múnera Bedoya</title>
+    <meta name="description" content="Hoja de Vida - John Edison Múnera Bedoya - Comunicador Social y Periodista - Universidad Pontificia Bolivariana">
+    <meta name="keywords" content="john edison munera bedoya, hoja de vida, comunicador, comuunicador social, periodista, upb, universidad pontificia bolivariana, especialista, profesional, marketing, comunicaciones, publicidad, periodismo, noticias, redes, blogs, gente, desarrollo web, redes sociales">
+    <meta name="author" content="k1n0com">
+    <meta name=”distribution” content=”global” />
+    <meta name="Robots" content="all" />
     <style>
         html {
             scroll-behavior: smooth;
@@ -13,6 +23,19 @@
     <link rel="stylesheet" href="assets/css/estilos.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;600&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/ico" href="assets/img/favicon.ico">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166831790-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-166831790-1');
+    </script>
 </head>
 
 <body>
